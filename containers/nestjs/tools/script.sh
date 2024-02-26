f5306869d8e7709108787acab06ce469cbeee1ac
root <root@DESKTOP-MIRRNAU>
2024-02-26 20:34:47 +0100

    good

--- containers/nestjs/tools/script.sh
+++ containers/nestjs/tools/script.sh
@@ -2,14 +2,13 @@
 
 cd  /home/back-end
 npm cache clean --force
-npm install -g npm@10.4.0
 npm install @prisma/client
 npm install --legacy-peer-deps
 npm install -g prisma
-npx prisma miigrate dev
 
-#npm run build
-#npm run start
-npm run start:dev
+
+npm run build
+npm run start
+
 
 
